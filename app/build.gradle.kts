211f9258ae8c009db16e54e4dbc53355ea46cd80
SociopathEliot <dlaonlcahik@gmail.com>
2025-06-30 02:13:58 +0600

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -40,6 +42,15 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.volley)
+
+    // Room
+    val room_version = "2.7.2"
+    implementation("androidx.room:room-runtime:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.56.2")
+    ksp("com.google.dagger:hilt-android-compiler:2.56.2")
 
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
---
+++ app/src/main/java/sl/kacinz/onluanmer/presentation/ui/fragments/main/AddTransactionFragment.kt
@@ -0,0 +1,27 @@
+package sl.kacinz.onluanmer.presentation.ui.fragments.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import sl.kacinz.onluanmer.R
+import sl.kacinz.onluanmer.databinding.FragmentAddTransactionBinding
+
+class AddTransactionFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddTransactionBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddTransactionBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
